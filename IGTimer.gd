6f27f23dff3ab66b8d3e1d1705eefe94ff17ed3e
tchy258 <tchy258@yahoo.cl>
2024-11-06 00:24:39 -0300

    feat: send commands to pause and resume livesplit igt upon entering/exiting scene
    Signed-off-by: tchy258 <tchy258@yahoo.cl>

--- IGTimer.gd
+++ IGTimer.gd
@@ -4,13 +4,25 @@ export var section := "section_name"
 
 var running := true
 
+func _exit_tree():
+	if IGT.has_peer():
+		IGT.send_command("pause_igt_command")
+	
+		
+
 func _ready() -> void:
 	Event.connect("beat_seraph_lumine",self,"stop")
 	running = true
+	call_deferred("resume_igt")
+
+func resume_igt():
+	if IGT.has_peer():
+		IGT.send_command("resume_igt_command")
 
 func _physics_process(delta: float) -> void:
 	if running:
 		IGT.add(section,delta)
 
 func stop() -> void:
+	IGT.send_command("split_command")
 	running = false
