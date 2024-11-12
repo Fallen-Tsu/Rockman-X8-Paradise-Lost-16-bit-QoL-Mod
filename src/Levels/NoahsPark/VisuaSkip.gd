fe6fd6b0ed3e6b821b11a632b91d4718678f0262
tchy258 <tchy258@yahoo.cl>
2024-11-12 00:28:21 -0300

    fix: skip indicator on intro now fills completely like before
    Signed-off-by: tchy258 <tchy258@yahoo.cl>

--- src/Levels/NoahsPark/VisuaSkip.gd
+++ src/Levels/NoahsPark/VisuaSkip.gd
@@ -2,14 +2,14 @@ extends Control
 
 onready var tween := TweenController.new(self,false)
 onready var progress: TextureProgress = $textureProgress
-
+export var fill_multiplier = 50
 var state := "none"
 
 func _ready() -> void:
 	visible = false
 
 func fill(timer) -> void:
-	progress.value = timer*50
+	progress.value = timer*fill_multiplier
 
 func fadeout() -> void:
 	if state != "fadeout":
