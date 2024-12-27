01027a0e9a13ef77bc29a60691941785b9f51e48
Tchy258 <tchy258@yahoo.cl>
2024-12-27 05:32:30 -0300

    fix: plasma shot pierces through everything

--- src/Actors/Weapons/Projectiles/PlasmaShot.gd
+++ src/Actors/Weapons/Projectiles/PlasmaShot.gd
@@ -1,11 +1,14 @@
-extends ChargedBuster
+extends SimplePlayerProjectile
 
 export var ball: PackedScene
 var current_balls := 0
 var enemies_hit : Array
+export var horizontal_velocity = 350
+export var vertical_position_range := 1
 
 func projectile_setup(dir : int, position : Vector2, _launcher_velocity := 0.0):
-	.projectile_setup(dir, position)
+	position_setup(position, dir)
+	set_horizontal_speed(dir * horizontal_velocity)
 	
 
 func create_ball(enemy):
@@ -15,3 +18,16 @@ func create_ball(enemy):
 		ball_instance.position = position
 		ball_instance.position.x += 32 * sign(self.velocity.x)
 		get_tree().current_scene.get_node("Objects").call_deferred("add_child",ball_instance,true)
+
+func _OnHit(_target_remaining_HP) -> void: #override
+	pass
+
+func _OnDeflect() -> void:
+	pass
+
+func position_setup(spawn_point:Vector2, direction:int):
+	var x = spawn_point.x 
+	var y = spawn_point.y
+	position.x = position.x + x * direction
+	position.y = position.y + y + int(rand_range(-vertical_position_range,vertical_position_range))
+	facing_direction = direction
