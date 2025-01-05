4de8c47392ba6e917e3f5c465bbbcf1f42777744
Tsu <fallen2kasa@gmail.com>
2025-01-05 16:53:54 +0100

    Updated from Alpha to Public Beta

--- src/Scripts/GameManager.gd
+++ src/Scripts/GameManager.gd
@@ -1,7 +1,7 @@
 extends Node
 
 const codename := "X8FC"
-const version := "1.7.21 - Fallen_Tsu's Mod 'Alpha'"
+const version := "2.0.0 - Fallen_Tsu's Mod 'Beta'"
 const current_demo := "Tchy's Speedrun Mod v7.-"
 
 var player : Character
